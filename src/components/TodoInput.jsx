157054b80c69e54950dee5e9e44dbd60b97ad03d
Sira Sasisakulporn <sira2violin@gmail.com>
2025-12-17 21:55:40 +0700

    add todo value by pressing enter

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,11 +3,21 @@ import { useState } from "react";
 export default function TodoInput(props) {
     const {handleAddTodos, todoValue, setTodoValue} = props;
 
+    function handleKeyDown(e) {
+        if (e.key === 'Enter') {
+            handleAddTodos(todoValue);
+            setTodoValue('');
+        }
+    }
+
     return (
         <header>
-            <input value={todoValue} onChange={(e) => {
-                setTodoValue(e.target.value)
-            }} placeholder="Enter todo..."/>
+            <input 
+                value={todoValue} onChange={(e) => {
+                    setTodoValue(e.target.value)
+                }} 
+                onKeyDown={handleKeyDown}
+                placeholder="Enter todo..."/>
             <button onClick={() => {
                 handleAddTodos(todoValue)
                 setTodoValue('')
